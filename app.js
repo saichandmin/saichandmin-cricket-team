ec38f8a14c466189f5ede3b28f7de8ec6ac2feff
saichand <159508319+saichandmin@users.noreply.github.com>
2024-05-28 18:10:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const {open} = require('sqlite')
-const sqlite3 = require('sqlite')
+const sqlite3 = require('sqlite3')
 const app = express()
 app.use(express.json())
 const dbpath = path.join(__dirname, 'cricketTeam.db')
